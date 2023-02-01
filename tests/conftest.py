c32785933b8b3b871d31641fe04c4ab5bbc7d749
monosans <hsyqixco@protonmail.com>
2023-02-01 22:15:50 +0300

    Monkeypatch pydantic in tests to make it stricter

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import pytest
+from pydantic import BaseConfig, Extra
+
+
+@pytest.fixture(autouse=True)
+def pydantic_strict(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(BaseConfig, "extra", Extra.forbid)
+    monkeypatch.setattr(BaseConfig, "validate_all", True)
+    monkeypatch.setattr(BaseConfig, "validate_assignment", True)
