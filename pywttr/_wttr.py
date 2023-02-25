798d3bb5691057311042b16b46c942f0c09eece6
monosans <hsyqixco@protonmail.com>
2023-02-25 21:56:57 +0300

    Replace pylint, pyupgrade and isort with ruff

--- pywttr/_wttr.py
+++ pywttr/_wttr.py
@@ -6,19 +6,15 @@ import pywttr_models
 from requests import Session
 
 
-# pylint: disable-next=too-many-public-methods
 class Wttr:
     """Wrapper for wttr.in weather forecast API."""
 
     __slots__ = ("location", "session")
 
-    def __init__(
-        self, location: str, session: Optional[Session] = None
-    ) -> None:
+    def __init__(self, location: str, session: Optional[Session] = None) -> None:
         self.location = location
         self.session = session
 
-    # pylint: disable=invalid-name
     def af(self) -> pywttr_models.af.Model:
         return pywttr_models.af.Model.parse_obj(self._get_json("af"))
 
@@ -70,7 +66,7 @@ class Wttr:
     def ia(self) -> pywttr_models.ia.Model:
         return pywttr_models.ia.Model.parse_obj(self._get_json("ia"))
 
-    def id(self) -> pywttr_models.id.Model:
+    def id(self) -> pywttr_models.id.Model:  # noqa: A003
         return pywttr_models.id.Model.parse_obj(self._get_json("id"))
 
     def it(self) -> pywttr_models.it.Model:
@@ -124,11 +120,9 @@ class Wttr:
     def zh_tw(self) -> pywttr_models.zh_tw.Model:
         return pywttr_models.zh_tw.Model.parse_obj(self._get_json("zh-tw"))
 
-    # pylint: enable=invalid-name
     def _fetch(self, lang: str, session: Session) -> Any:
         with session.get(
-            f"https://wttr.in/{self.location}",
-            params={"format": "j1", "lang": lang},
+            f"https://wttr.in/{self.location}", params={"format": "j1", "lang": lang}
         ) as response:
             response.raise_for_status()
             return response.json()
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from pydantic import BaseConfig, Extra
 
 
 @pytest.fixture(autouse=True)
-def pydantic_strict(monkeypatch: pytest.MonkeyPatch) -> None:
+def _pydantic_strict(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr(BaseConfig, "extra", Extra.forbid)
     monkeypatch.setattr(BaseConfig, "validate_all", True)
     monkeypatch.setattr(BaseConfig, "validate_assignment", True)
